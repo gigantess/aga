60bacc81c530eabbc09273ffe7a4de48214ed122
KSH <true9090@gmail.com>
2025-09-30 14:21:18 +0900

    chore: init project with express + ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+﻿import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello AGA API!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
