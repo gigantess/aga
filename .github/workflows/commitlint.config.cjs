d03bf5ba272350dd1b589e3bb5140028366ff57b
KSH <true9090@gmail.com>
2025-09-30 15:24:16 +0900

    invalid message

--- .github/workflows/commitlint.config.cjs
+++ .github/workflows/commitlint.config.cjs
@@ -2,9 +2,9 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', [
-      'feat','fix','docs','style','refactor','test','chore','ci','perf','build','revert'
+      'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'ci', 'perf', 'build', 'revert'
     ]],
-    'subject-case': [2, 'never', ['sentence-case','start-case','pascal-case','upper-case']],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'header-max-length': [2, 'always', 100]
   }
 };
